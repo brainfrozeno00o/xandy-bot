09d71efd25822eac26ae3697196f2548b6a454dd
Elmo Lingad <31301984+brainfrozeno00o@users.noreply.github.com>
2021-11-13 09:44:24 +0800

    XDY-013: Added the execute command for the recent script

--- alembic/versions/77ee8cc881e2_add_images_v1.py
+++ alembic/versions/77ee8cc881e2_add_images_v1.py
@@ -18,19 +18,22 @@ depends_on = None
 
 def upgrade():
     # insert initial image links
-    """INSERT INTO all_images (image_link) VALUES
-        ('https://media.discordapp.net/attachments/360290757610962954/893757713664852038/xander.png'),
-        ('https://media.discordapp.net/attachments/360290757610962954/893757785987231794/unknown_1.png'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901420249101008936/NGVL7394.JPG?width=450&height=676'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901420323390496788/IMG_6417.JPG'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901420635207655474/IMG_5947.JPG'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901421757972480020/unknown.png?width=524&height=676'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901424690000691280/IMG_4499.jpg?width=901&height=676'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901425135075082260/IMG_6210.jpg?width=508&height=676'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901425149847437332/IMG_5742.jpg?width=508&height=676'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901425183460581386/IMG_3711.jpg?width=507&height=676'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901425227723075604/IMG_3661.jpg?width=507&height=676'),
-        ('https://media.discordapp.net/attachments/893759325393289256/901425241333592114/IMG_5756.jpg?width=901&height=676')"""
+    op.execute(
+        """INSERT INTO all_images (image_link) VALUES
+            ('https://media.discordapp.net/attachments/360290757610962954/893757713664852038/xander.png'),
+            ('https://media.discordapp.net/attachments/360290757610962954/893757785987231794/unknown_1.png'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901420249101008936/NGVL7394.JPG?width=450&height=676'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901420323390496788/IMG_6417.JPG'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901420635207655474/IMG_5947.JPG'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901421757972480020/unknown.png?width=524&height=676'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901424690000691280/IMG_4499.jpg?width=901&height=676'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901425135075082260/IMG_6210.jpg?width=508&height=676'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901425149847437332/IMG_5742.jpg?width=508&height=676'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901425183460581386/IMG_3711.jpg?width=507&height=676'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901425227723075604/IMG_3661.jpg?width=507&height=676'),
+            ('https://media.discordapp.net/attachments/893759325393289256/901425241333592114/IMG_5756.jpg?width=901&height=676')"""
+    )
+
 
 
 def downgrade():
